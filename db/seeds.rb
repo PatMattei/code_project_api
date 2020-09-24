58604cab87ad3d1413dc1b1835d9e8fe4cf41d79
Patrick Mattei <patrick.mattei@gmail.com>
2020-09-24 12:37:09 -0400

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+    Post.create(
+        title: Faker::Hipster.sentence(word_count: 3),
+        content: Faker::Lorem.paragraph,
+        code: "print 'Hello World'",
+        language: Faker::ProgrammingLanguage.name,
+        day: DateTime.new(2009,9,1,17)
+    )
+end
